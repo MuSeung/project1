465d1833a634699656d20afebea05b50c57d0587
BHGoo <gbh1055@naver.com>
2022-12-28 18:25:21 +0900

    구본홍 푸쉬

---
+++ src/main/java/com/shop/dto/Adm.java
@@ -0,0 +1,18 @@
+package com.shop.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Adm {
+	private String id;
+	private String pwd;
+	private int lev;
+}
