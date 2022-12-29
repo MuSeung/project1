f096a222e507d7f6fff78f938b7bf0a7bd408eef
JiwonPark <king.jiwon.park@gmail.com>
2022-12-29 16:24:04 +0900

    카테고리 검색 기능 추가

--- src/main/java/com/shop/mapper/CategoryMapper.java
+++ src/main/java/com/shop/mapper/CategoryMapper.java
@@ -1,5 +1,7 @@
 package com.shop.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,8 @@ import com.shop.frame.MyMapper;
 @Repository
 @Mapper
 public interface CategoryMapper extends MyMapper<Integer, Category>{
+
+	//카테고리검색
+	public List<Category> searchcate(String txt) throws Exception;
 	
 }
